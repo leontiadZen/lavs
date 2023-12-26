5bbe26c0f42adc92d28c9fc31eb64977dccd5f5a
Iraklis Leontiadis <120665504+leontiadZen@users.noreply.github.com>
2023-12-26 12:59:11 +0000

    without agg_verify

--- src/basic_lavs.rs
+++ src/basic_lavs.rs
@@ -7,20 +7,17 @@ use curv::elliptic::curves::bls12_381::g2::GE as GE2;
 use curv::elliptic::curves::bls12_381::Pair;
 use curv::elliptic::curves::traits::{ECPoint, ECScalar};
 use curv::BigInt;
+use curv::arithmetic::Converter;
+
 use ff_zeroize::Field;
+
 use pairing_plus::bls12_381::{Fq12, G1Affine};
 use pairing_plus::serdes::SerDes;
-use curv::arithmetic::Converter;
 
 #[derive(Clone, Copy, Debug)]
 pub struct KeyPairG2 {
     Y: GE2,
     x: FE2,
-    B: i32
-}
-
-pub struct VK{
-
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -32,7 +29,7 @@ impl KeyPairG2 {
     pub fn new(messages:i32) -> Self {
         let x: FE2 = ECScalar::new_random();
         let Y = GE2::generator() * &x;
-        KeyPairG2 { x, Y,B:messages }
+        KeyPairG2 { x, Y }
     }
 }
 
@@ -90,9 +87,9 @@ impl LAVSSignature {
                 //x[l]-x[j]
 				let diff_1 =  BigInt::from_bytes(&messages[j]) - BigInt::from_bytes(&messages[i]);
                 let diff_1_scalar: FE1 = ECScalar::from(&diff_1);
+                
                 //1/(x[l]-x[j])
                 let tmp_invert = diff_1_scalar.invert();
-
 
                 //P[j]-P[l]
                 let mut neg_sig2 = signatures[j].sigma.bytes_compressed_to_big_int();
@@ -121,6 +118,69 @@ impl LAVSSignature {
         LAVSSignature::DPP(messages,signatures)
 
      }
+
+    //  pub fn compute_betas_coeff(messages: Vec<Vec<u8>>)->Vec<FE2>{
+    //     //TODO. Not clear to me yet what the y's value should be to interpolate. Is Lagrange needed here?
+    //  }
+
+    //  pub fn agg_verify(vk: &GE1,messages: Vec<Vec<u8>>)-> bool {
+    //     //TODO once compute_betas_coeff is ready 
+    //     true
+
+    //  }
+
+    //  pub fn local_open(vk: &GE1,messages: Vec<Vec<u8>>,j:i32)->(&GE2, &GE2){
+    //     //TODO once compute_betas_coeff is ready 
+
+    //  }
+
+     pub fn LocalAggVerify(message: &[u8], aggregate_sig: GE1,aux1: &GE2,aux2: &GE2,vk: &GE1)->bool{
+        //message as BigInt
+        let mes = BigInt::from_bytes(message);
+        //message  in G2
+        let mes_f1: FE2 = ECScalar::from(&mes);
+
+        //aux1^msg*aux2
+        let el1_2  = aux1 * &mes_f1 + aux2;
+
+        //e(aggregate_sig,aux1^msg*aux2)
+        let el1 = Pair::compute_pairing(&aggregate_sig,  &el1_2);
+        //e(g1,g2)
+        let el2 = Pair::compute_pairing(&GE1::generator(), &(GE2::generator()));
+        //e(g1^a,aux1)
+        let el3 = Pair::compute_pairing(&vk, &(aux1));
+        //e(g1,aux2)
+        let el4 = Pair::compute_pairing(&GE1::generator(), &(aux2));
+
+        el1==el2 && el3==el4
+
+
+
+     }
+
+     pub fn SeqAggSign(message: &[u8],messages: Vec<Vec<u8>>,aggregate_sig: GE1,keys: &KeyPairG2)->GE1{
+        //the first step is to agg_verify which is not implemented yet.
+         
+        //x in G1
+        let fe1_x: FE1 = ECScalar::from(&ECScalar::to_big_int(&keys.x));
+        //message as BigInt
+        let mes = BigInt::from_bytes(message);
+        //message  in G1
+        let mes_f1: FE1 = ECScalar::from(&mes);
+        //1/(a+m)
+        let exp: FE1 = (mes_f1 + fe1_x).invert();
+        //aggregate_sig^{1/(a+m)}
+        aggregate_sig * exp
+         
+
+
+     }
+     pub fn SeqAggVerify(message: &[u8], messages: Vec<Vec<u8>>,keys: &KeyPairG2,aggregate_sig: GE1,){
+        //TODO:  blocked  by agg_verify->compute_betas_coeff
+
+
+
+     }
  }
 
 mod test {
@@ -145,5 +205,15 @@ mod test {
         let signature = LAVSSignature::sign(&message_bytes[..], &keypair);
         assert!(signature.verify(&message_bytes[..], &Y));
     }
+
+    // pub fn test_aggregate_lavs_signature() {
+    //     let messages_bytes:Vec<Vec<i32>> = vec![["transfer 3 monads"],[353],[2],[5]];
+    //     let a:Vec<Vec<i32>>;
+    //     let keypair = KeyPairG2::new(3);
+    //     let Y = keypair.Y.clone();
+        
+    //     let signature = LAVSSignature::sign(&message_bytes[..], &keypair);
+    //     assert!(signature.verify(&message_bytes[..], &Y));
+    // }
 
 }
